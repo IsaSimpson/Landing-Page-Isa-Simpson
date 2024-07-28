73a34968c793dd599fe85016453271ecdf5ecada
IsaSimpson <167341295+IsaSimpson@users.noreply.github.com>
2024-07-28 20:29:26 +0000

    Landing-Page-Isa

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,17 @@
+
 //import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { appName, logoUrl } from "./component/appName.js";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Jumbotron from "./component/Jumbotron.jsx";
+import Cards from "./component/Cards.jsx";
+
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
-
+ReactDOM.render(<Home appName= {appName}  logoUrl={logoUrl}/>, document.querySelector("#app"));
